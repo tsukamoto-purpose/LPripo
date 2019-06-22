52c0dc67a605093e4b0da7ad23d33ccf6cde73f8
ueda hiroto <pyonns.r@icloud.com>
2019-06-23 08:57:02 +0900

    6/22
    sample

--- the_purpose/index.js
+++ the_purpose/index.js
@@ -1,14 +1,22 @@
-$(function() {
+var hhhhh = document.querySelectorAll('.sa');
+var animation = function() {
+  for(var i = 0; i < hhhhh.length; i++) {
+    var trigger = 150;
+    if (window.innerHeight > hhhhh[i].getBoundingClientRect().top + trigger) {
+      hhhhh[i].classList.add('show');
+    }
+  }
+}
+window.addEventListener('load', animation);
+window.addEventListener('scroll', animation);
 
+
+// 以下jquery
+$(function() {
   $('.map-show').click(function(){
     $('#map').fadeIn();
   });
-
-  // 「.close-modal」要素にclickイベントを設定してください
   $('.close-map').click(function(){
     $('#map').fadeOut();
-
   });
-
-
 });
