1740220a2c089685733b4ee5a2384133012a77ea
ueda hiroto <pyonns.r@icloud.com>
2019-06-23 07:16:12 +0900

    secound comitt

---
+++ the_purpose/index.js
@@ -0,0 +1,14 @@
+$(function() {
+
+  $('.map-show').click(function(){
+    $('#map').fadeIn();
+  });
+
+  // 「.close-modal」要素にclickイベントを設定してください
+  $('.close-map').click(function(){
+    $('#map').fadeOut();
+
+  });
+
+
+});
