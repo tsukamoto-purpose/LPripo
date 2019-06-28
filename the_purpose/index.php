316241debb1392a22041f2a2fbbadb5782980b1d
ueda hiroto <pyonns.r@icloud.com>
2019-06-28 14:58:34 +0900

    sample
    6/27

---
+++ the_purpose/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>sample</title>
+</head>
+
+<body>
+
+<h2>問合せ内容</h2>
+
+<form action="mailto.php" method="post">
+
+  <table border="1">
+    <tr>
+      <td>名前</td>
+      <td><?php echo $_POST["name"]; ?></td>
+    </tr>
+    <tr>
+      <td>TEL</td>
+      <td><?php echo $_POST["tel"]; ?></td>
+    </tr>
+    <tr>
+      <td>メールアドレス</td>
+      <td><?php echo $_POST["mail"]; ?></td>
+    </tr>
+    <tr>
+      <td>会社名</td>
+      <td><?php echo $_POST["company"]; ?></td>
+    </tr>
+    <tr>
+      <td>題名</td>
+      <td><?php echo $_POST["section"]; ?></td>
+    </tr>
+    <tr>
+      <td>問い合わせ内容</td>
+      <td><?php echo $_POST["inquiry"]; ?></td>
+    </tr>
+  </table>
+
+  <input type="submit" value="送信" />
+</form>
+
+</body>
+
+</html>
