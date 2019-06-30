48030acbfcc850e0112b36b9998e6c7161102de4
ueda hiroto <pyonns.r@icloud.com>
2019-06-30 11:44:41 +0900

    mk

--- the_purpose/index.js
+++ the_purpose/index.js
@@ -1,5 +1,33 @@
+var hhhhh = document.querySelectorAll('.sa');
+var animation = function() {
+  for(var i = 0; i < hhhhh.length; i++) {
+    var trigger = 100;
+    if (window.innerHeight > hhhhh[i].getBoundingClientRect().top + trigger) {
+      hhhhh[i].classList.add('show');
+    }
+  }
+}
+window.addEventListener('load', animation);
+window.addEventListener('scroll', animation);
 
 
+
+var line = document.querySelectorAll('.rara');
+var punch = function() {
+  for(var i = 0; i < line.length; i++) {
+    var samsam = 150;
+    if (window.innerHeight > line[i].getBoundingClientRect().top + samsam) {
+      line[i].classList.add('taken');
+    }
+  }
+}
+window.addEventListener('load', punch);
+window.addEventListener('scroll', punch);
+
+
+
+
+//メニューバーを指定場所から表記
 $(function() {
     var topBtn = $('#fifi');
     topBtn.hide();
@@ -31,33 +59,14 @@ const delay = 150 //1文字が表示される時間
 document.getElementById("typing").innerHTML = "";
 window.setInterval(function(){typing(str);}, delay);
 
-//以下6/30に。62html
-// function following(str = ""){
-//     let shushu = document.getElementById("following").innerHTML; //書き込み済みの文字を要素から取得
-//     let writed = shushu.length; //書き込み済みの文字数を取得
-//     let write = "";
-//     if(writed < str.length){
-//         write = str.charAt(writed); //1文字だけ取得する
-//     }else{
-//         shushu = shushu; //文字の消去("")で際表記
-//     }
-//     document.getElementById("following").innerHTML = shushu + write; //1文字だけ追加していく
-// }
-//
-// const string = document.getElementById("following").innerHTML; //書き込む文字を要素から取得
-// const deldel = 150 //1文字が表示される時間
-//
-// document.getElementById("following").innerHTML = "";
-// window.setInterval(function(){following(string);}, deldel);
-//
 
 
 // 遅らせる
 $(function(){
-    $(".kakkoii").css({opacity:'0'});
+    $(".delay").css({opacity:'0'});
     setTimeout(function(){
-        $(".kakkoii").stop().animate({opacity:'1'},1300);
-    },1400);
+        $(".delay").stop().animate({opacity:'1'},2000);
+    },2100);
 });
 
 
@@ -82,7 +91,7 @@ function rect() {
 
 
  $(function(){
-   // #で始まるリンクをクリックしたら実行されます
+   // #で始まるリンクをクリックしたら実行される
    $('a[href^="#"]').click(function() {
      // スクロールの速度
      var speed = 400; // ミリ秒で記述
